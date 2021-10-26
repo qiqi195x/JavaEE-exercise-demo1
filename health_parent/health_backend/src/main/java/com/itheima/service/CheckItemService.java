73f3c9b8a4b05e629a88c184475e0143beaee9b0
“chenzunming” <841832010@qq.com>
2021-10-26 18:06:59 +0800

    10.26 后端数据库

---
+++ health_parent/health_backend/src/main/java/com/itheima/service/CheckItemService.java
@@ -0,0 +1,7 @@
+package com.itheima.service;
+
+import com.itheima.pojo.CheckItem;
+
+public interface CheckItemService {
+    public void add(CheckItem checkItem);
+}
---
+++ health_parent/health_service_provider/src/main/java/com/itheima/dao/CheckItemDao.java
@@ -0,0 +1,7 @@
+package com.itheima.dao;
+
+import com.itheima.pojo.CheckItem;
+
+public interface CheckItemDao {
+    public void add(CheckItem checkItem);
+}
