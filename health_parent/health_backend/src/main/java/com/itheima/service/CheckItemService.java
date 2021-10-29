3416b4b3793e28de127b47671418e52509666e08
“chenzunming” <841832010@qq.com>
2021-10-29 10:35:59 +0800

    new

--- health_parent/health_backend/src/main/java/com/itheima/service/CheckItemService.java
+++
@@ -1,7 +0,0 @@
-package com.itheima.service;
-
-import com.itheima.pojo.CheckItem;
-
-public interface CheckItemService {
-    public void add(CheckItem checkItem);
-}
--- health_parent/health_service_provider/src/main/java/com/itheima/dao/CheckItemDao.java
+++
@@ -1,7 +0,0 @@
-package com.itheima.dao;
-
-import com.itheima.pojo.CheckItem;
-
-public interface CheckItemDao {
-    public void add(CheckItem checkItem);
-}
